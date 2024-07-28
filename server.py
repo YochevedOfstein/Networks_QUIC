e434ac34e28333f5b623c44f233ca967ad8f2952
mor <morberger444@gmail.com>
2024-07-28 16:13:20 +0300

    mor

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from quic import quic_recv, quic_close, print_statistics
 
+
 def server_function():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     HOST = '127.0.0.1'
@@ -11,11 +12,13 @@ def server_function():
     print("Server listening")
     stream_ids = list(range(1, 11))
 
+    run = True
     try:
-        while True:
+        while run:
             for stream_id in stream_ids:
                 result = quic_recv(sock)
-                if result == "close":
+                if result == ('close', None, None):
+                    run = False
                     break
     except KeyboardInterrupt:
         pass
@@ -23,6 +26,7 @@ def server_function():
         print(f"Error receiving packet: {e}")
 
     print_statistics()
+
 
 if __name__ == '__main__':
     server_function()
